07d7830b1f06a8405aa2d7ad3b6b89f37b7b4b65
henrikraakil <lhraakil@uia.no>
2022-02-25 16:11:27 +0100

    update

--- modul7/FixedArray.java
+++ modul7/FixedArray.java
@@ -1,9 +1,11 @@
 public class FixedArray
 {
    private String[] arrayFood;
+   private float[] arrayFloat;
     public FixedArray()
     {
         arrayFood = new String[6];
+        arrayFloat = new float[] {0.1f, 0.2f, 0.3f};
         arrayFood[0] = "Apple"; arrayFood[1] = "Banana";
         arrayFood[2] = "Mango"; arrayFood[3] = "Potato";
         arrayFood[4] = "Carrot"; arrayFood[5] = "Pear"; 
@@ -13,9 +15,10 @@ public class FixedArray
     {
         System.out.println(arrayFood[0]); System.out.println(arrayFood[1]);
         System.out.println(arrayFood[2]); System.out.println(arrayFood[3]);
+        System.out.println(arrayFloat[0]);
     }
     
-    public void conditionalOperatorExample(int b)
+    public void conditionalOperator(int b)
     {
         int a = 10;
         if (a == b) {
@@ -24,6 +27,12 @@ public class FixedArray
         else {
             System.out.println(a + " og " + b + " Har ikke samme verdi");
         }
+    }
+    
+    public void ternaryOperator()
+    {
+        int var1 = 1; int var2 = 1;
+        System.out.println(var1 == var2 ? "yes" : "no");
     }
     
     public void forLoop()
